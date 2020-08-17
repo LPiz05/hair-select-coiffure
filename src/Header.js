df342b637145cacb11fa97d1befc1f0986579f10
Luis <pereiravazluis@gmail.com>
2020-08-17 17:46:57 +0200

    Ajout du composant Header

---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./styles.css";
+import logo from "./logo.jpg";
+
+export default function Header() {
+  return (
+    <header className="Header">
+      <nav>
+        <ul className="topnav">
+          <li>
+            <img src={logo} alt="Logo" />
+          </li>
+          <li>
+            <a href="#">Qui ?</a>
+          </li>
+          <li>
+            <a href="#">Quoi ?</a>
+          </li>
+          <li>
+            <a href="#">Où ?</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
