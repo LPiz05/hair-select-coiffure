94e25327cb9c07795b12ace14081e885e98c5393
Luis <pereiravazluis@gmail.com>
2020-08-17 23:45:44 +0200

    Correctif code Header

--- src/Header.js
+++ src/Header.js
@@ -7,18 +7,10 @@ export default function Header() {
     <header className="Header">
       <nav>
         <ul className="topnav">
-          <li>
-            <img src={logo} alt="Logo" />
-          </li>
-          <li>
-            <a href="#">Qui ?</a>
-          </li>
-          <li>
-            <a href="#">Quoi ?</a>
-          </li>
-          <li>
-            <a href="#">Où ?</a>
-          </li>
+          <li><img src={logo} alt="Logo" /></li>
+          <li><a href="#">Qui ?</a></li>
+          <li><a href="#">Quoi ?</a></li>
+          <li><a href="#">Où ?</a></li>
         </ul>
       </nav>
     </header>
