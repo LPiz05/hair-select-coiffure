e9c72fbcd33abb5f4434f0e0019f48a4227e46a7
Luis <pereiravazluis@gmail.com>
2020-08-21 23:58:10 +0200

    Add css and icon FontAwesome in Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,25 +1,30 @@
 import React from "react";
-import { FaRegEnvelope } from "react-icons/fa";
-import { AiOutlineFacebook, AiOutlineYoutube } from "react-icons/ai";
-import { MdPhoneIphone } from "react-icons/md";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faFacebook, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faMobileAlt } from "@fortawesome/free-solid-svg-icons";
 import "./styles.css";
 
 export default function Footer() {
   return (
     <div className="Footer">
-      <div class="p-3 bg-dark text-white">
-        <a href="/">
-          <FaRegEnvelope />
-        </a>
-        <a href="/">
-          <AiOutlineFacebook />
-        </a>
-        <a href="/">
-          <MdPhoneIphone />
-        </a>
-        <a href="/">
-          <AiOutlineYoutube />
-        </a>
+      <div className="p-3 bg-dark text-white">
+        <div className="float-left">
+          <a href="mailto:coiffure@gmail.com">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+          <a href="tel:0198989898">
+            <FontAwesomeIcon icon={faMobileAlt} />
+          </a>
+        </div>
+        <div className="text-right">
+          <a href="https://www.facebook.com" target="_blank">
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+          <a href="https://www.youtube.com" target="_blank">
+            <FontAwesomeIcon icon={faYoutube} />
+          </a>
+        </div>
       </div>
     </div>
   );
