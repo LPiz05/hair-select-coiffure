a185db03d6b73aba3a66c8bd6e58d52c99e0d5a9
Helder Salvador <helder.salvador@yahoo.com>
2020-09-15 21:31:17 +0100

    Update header + maps

--- client/src/Content.js
+++ client/src/Content.js
@@ -9,51 +9,36 @@ import image1 from "./img/1.png";
 import image2 from "./img/2.png";
 import image3 from "./img/3.png";
 import image4 from "./img/4.png";
-import Map from "./Map";
-import places from "./places.json";
+import image_maps from "./img/maps.png"
+import LoremIpsum from "./Loremipsum";
 
 export default function Content() {
   return (
     <React.Fragment>
       <div className="container row col-md-12 col-sm-12">
-        <Element name="who" className="element col-md-6">
+        <Element name="who" className="element col-md-12">
           <div className="p-2">
             <h1>Qui</h1>
-            <p className="border border-primary rounded p-2">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+            <p className="border border-primary rounded p-2 col-md-6 float-left">
+              <LoremIpsum />
             </p>
             <img
               src={salon}
               alt="Logo"
-              className="rounded mx-auto d-block col-md-12"
+              className="rounded mx-auto d-block col-md-6 float-right"
             />
           </div>
         </Element>
-        <Element name="what" className="element col-md-6">
+      </div>
+
+      <div className="container row col-md-12 col-sm-12">
+        <Element name="what" className="element col-md-12">
           <div className="p-2">
             <h1>Quoi</h1>
             <p className="border border-primary rounded p-2">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+              <LoremIpsum />
             </p>
-            <AliceCarousel autoPlay autoPlayInterval="3000">
+            <AliceCarousel autoPlayInterval="3000">
               <img src={image1} className="sliderimg" alt="" />
               <img src={image2} className="sliderimg" alt="" />
               <img src={image3} className="sliderimg" alt="" />
@@ -62,29 +47,28 @@ export default function Content() {
           </div>
         </Element>
       </div>
-      <Element name="where" className="element">
-        <div className="p-2">
-          <h1>Où</h1>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
-          <div>
-            <Map
-              center={{ lat: 48.8553603, lng: 2.3460132 }} //centrer la carte sur où on veut montrer
-              zoom={13} //zoom sur la carte
-              places={places} //les différentes places que l'on veut afficher
-            />
+
+      <div className="container row col-md-12 col-sm-12">
+
+        <Element name="where"  className="element col-md-12">
+          <div className="p-2" >
+            <h1>Où</h1>
+            <p className="border border-primary rounded p-2 col-md-6 float-left">
+              <LoremIpsum />
+            </p>
+            <div className="rounded mx-auto d-block col-md-6 float-right">
+              <a href="https://www.google.com/maps/place/Avenue+du+G%C3%A9n%C3%A9ral+de+Gaulle,+94420+Le+Plessis-Tr%C3%A9vise/@48.8097452,2.5727334,17.25z/data=!4m5!3m4!1s0x47e60ef9b6f7519f:0xc51b562cb91cbbfc!8m2!3d48.8079608!4d2.5785944">
+                <img 
+                  src={image_maps} 
+                  alt="Emplacement hair select coiffure"
+                   />
+              </a>
+            </div>
           </div>
-        </div>
-      </Element>
+        </Element>
+      </div>
+     
+     
     </React.Fragment>
   );
 }
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,54 +1,66 @@
 import React from "react";
 import { Link } from "react-scroll";
-import "./styles.css";
-import logo from "./img/logo.jpg";
+import "./header.css";
+import { Navbar, Form, Nav, NavDropdown, FormControl, Button } from "react-bootstrap";
+import {logo} from "./img/elina.jpg";
 
 export default function Header() {
   return (
-    <header className="Header">
-      <nav>
-        <ul className="topnav">
-          <li>
-            <img src={logo} alt="Logo" />
-          </li>
-          <li>
-            <Link
-              activeClass="active"
-              className="who"
-              to="who"
-              spy={true}
-              smooth={true}
-              duration={500}
-            >
-              Qui ?
-            </Link>
-          </li>
-          <li>
-            <Link
-              activeClass="active"
-              className="what"
-              to="what"
-              spy={true}
-              smooth={true}
-              duration={500}
-            >
-              Quoi ?
-            </Link>
-          </li>
-          <li>
-            <Link
-              activeClass="active"
-              className="where"
-              to="where"
-              spy={true}
-              smooth={true}
-              duration={500}
-            >
-              Où ?
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <Navbar className="setShadow" fixed="top" bg="light" expand="lg">
+                <div className="container">
+                    <Navbar.Brand href="#header">Hair Select Coiffure</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="#association">Qui?</Nav.Link>
+                            <Nav.Link href="#application">Où?</Nav.Link>
+                            <Nav.Link href="#contact">Quoi?</Nav.Link>
+                        </Nav>
+                       
+                    </Navbar.Collapse>
+                </div>
+            </Navbar>
   );
 }
+
+// {/* <ul className="topnav">
+// <li>
+//   <p> Hair Select coiffure </p>
+// </li>
+// <li>
+//   <Link
+//     activeClass="active"
+//     className="who"
+//     to="who"
+//     spy={true}
+//     smooth={true}
+//     duration={500}
+//   >
+//     Qui ?
+//   </Link>
+// </li>
+// <li>
+//   <Link
+//     activeClass="active"
+//     className="what"
+//     to="what"
+//     spy={true}
+//     smooth={true}
+//     duration={500}
+//   >
+//     Quoi ?
+//   </Link>
+// </li>
+// <li>
+//   <Link
+//     activeClass="active"
+//     className="where"
+//     to="where"
+//     spy={true}
+//     smooth={true}
+//     duration={500}
+//   >
+//     Où ?
+//   </Link>
+// </li>
+// </ul> */}
